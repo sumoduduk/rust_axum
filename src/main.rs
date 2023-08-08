709b69f726e8e2a52a520ca2a0b68fbc16e2081d
sumoduduk <sumoduduk12@gmail.com>
2023-08-07 22:55:46 -0500

    update

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,32 @@
-use db::{Operation, OperationResult};
+use axum::{routing::get, Router};
 use dotenv::dotenv;
-use sqlx::{
-    postgres::PgPoolOptions,
-    types::chrono::{DateTime, Utc},
-    Pool, Postgres,
-};
-use std::env;
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+use std::{env, net::SocketAddr};
 
-mod db;
+mod ipfs_router;
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
-    let pool: Pool<Postgres> = PgPoolOptions::new()
+    let _pool: Pool<Postgres> = PgPoolOptions::new()
         .max_connections(5)
         .connect(&database_url)
         .await
         .unwrap();
 
-    let res = Operation::Read.execute(&pool).await.unwrap();
+    let app = Router::new().route("/", get(home));
 
-    if let OperationResult::ArrStruct(data) = res {
-        println!("{:?}", data);
-    };
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Listening on {addr}");
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
 
-fn parse_timestamp(timestamp: &str) -> Option<DateTime<Utc>> {
-    DateTime::parse_from_rfc3339(timestamp)
-        .ok()
-        .map(|dt| dt.with_timezone(&Utc))
+async fn home() -> String {
+    "Hello World".to_string()
 }
